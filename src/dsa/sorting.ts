ac766792efbabb5b42097705c9987cd087dd1a6e
Akshayaap <Akshaykumarparmar51@gmail.com>
2022-11-30 01:20:15 +0530

    bubble sort

--- src/dsa/sorting.ts
+++ src/dsa/sorting.ts
@@ -48,12 +48,12 @@ export const play: () => void = () => {
 export const renderBars: (barCount: number) => void = (barCount: number) => {
     let htmlString: string = '';
     for (let i: number = 0; i < barCount; i++) {
-        htmlString += `<div  id="${i}" class="bar"></div>`;
+        htmlString += `<div  id="bar-${i}" class="bar"></div>`;
     }
     $('#canvas .array').html(htmlString);
 };
 
-class Sorting {
+export class Sorting {
     size: number;
     array: Array<number>;
     elements: Array<JQuery>;
@@ -82,19 +82,57 @@ class Sorting {
     }
 
     generateArray() {
-
+        this.array = [];
+        for (let i = 0; i < this.size; i++) {
+            this.array.push(Math.floor(Math.random() * 800) + 1);
+        }
     }
 
     fetchElements() {
-
+        for (let i = 0; i < this.size; i++) {
+            this.elements.push($(`#bar-${i}`));
+            this.elements[i].css({height: `${this.array[i]}px`});
+            this.elements[i].css({width: `${80 / this.size}%`});
+        }
     }
 
     shuffle() {
-
+        this.generateArray();
+        for (let i = 0; i < this.size; i++) {
+            this.elements[i].css({height: `${this.array[i]}px`});
+            this.elements[i].css({background: `#00ffff`});
+        }
     }
 
-    bubbleSort() {
+    async bubbleSort() {
+        this.isRunning = true;
+        //bubble sort algorithm with await function
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size - i - 1; j++) {
+                if (this.array[j] > this.array[j + 1]) {
+                    let temp = this.array[j];
+                    this.array[j] = this.array[j + 1];
+                    this.array[j + 1] = temp;
+                    this.elements[j].css({height: `${this.array[j]}px`});
+                    this.elements[j + 1].css({height: `${this.array[j + 1]}px`});
 
+
+                    //change colors of bars
+                    this.elements[j].css({background: `#00ff00`});
+                    this.elements[j + 1].css({background: `#00ff00`});
+
+                    await sleep(50);
+                    //revert back colors
+                    this.elements[j].css({background: `#00ffff`});
+                    this.elements[j + 1].css({background: `#00ffff`});
+                }
+            }
+            //change color for sorted part of an array
+            for (let k = this.size - 1; k >= this.size - i - 1; k--) {
+                this.elements[k].css({background: `#ff0000`});
+            }
+        }
+        this.isRunning = false;
     }
 
     mergeSort() {
@@ -130,6 +168,11 @@ class Sorting {
     }
 
     isSorted() {
-
+        for (let i = 0; i < this.array.length; i++) {
+            if (this.array[i] > this.array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,10 @@
-import {renderBars} from "./dsa/sorting";
+import {renderBars, Sorting} from "./dsa/sorting";
 
 
 export const init: () => void = async () => {
     renderBars(50);
+    const sim = new Sorting(50);
+    sim.fetchElements();
+    sim.generateArray();
+    let promise = sim.bubbleSort();
 }
